b903061f59d68da36582bb01161c82e07dcff424
yogajunky <yogajunky@nyu>
2012-06-05 17:35:21 -0400

    Initial Commit

---
+++ Jun7/Jun7AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Jun7AppDelegate.h
+//  Jun7
+//
+//  Created by Michael Grubb on 6/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Jun7AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Jun7/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Jun7
+//
+//  Created by Michael Grubb on 6/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "Jun7AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([Jun7AppDelegate class]));
+    }
+}
