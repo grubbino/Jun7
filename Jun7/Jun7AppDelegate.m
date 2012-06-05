c8212b52a34ec8c93dd66da0916b1e5096d307a1
Michael Grubb <michael.grubb@nyu.edu>
2012-06-05 18:31:23 -0400

    first commit

--- Jun7/Jun7AppDelegate.m
+++ Jun7/Jun7AppDelegate.m
@@ -16,7 +16,7 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
-    self.window.backgroundColor = [UIColor whiteColor];
+    self.window.backgroundColor = [UIColor yellowColor];
     [self.window makeKeyAndVisible];
     return YES;
 }
--- Jun7/main.m
+++ Jun7/main.m
@@ -13,6 +13,17 @@
 int main(int argc, char *argv[])
 {
     @autoreleasepool {
+        UIDevice *device = [UIDevice currentDevice];
+        NSString *version = device.systemVersion;
+        NSString *name = device.name;
+        NSLog(@"The version of iOS is %@",version);
+        NSLog(@"The name of this device is %@",name);
+        BOOL multiTask = device.isMultitaskingSupported;
+        if (multiTask) {
+            NSLog(@"Multitasking is supported on this device.");
+        } else { 
+            NSLog(@"Multitasking is not supported on this device.");
+        }
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([Jun7AppDelegate class]));
     }
 }
